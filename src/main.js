b82d961ff3975fce62c7bb257be1b33bc3421e52
Ever G <edgg72@hotmail.com>
2026-01-05 21:25:27 -0500

    test modern husky hook

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+  alert("Button clicked!");
+});
+
+//we can use this command to output a report
+// npx eslint src --format json --output-file eslint-report.json
